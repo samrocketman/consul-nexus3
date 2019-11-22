f969b7a4953125c3c073d11408457d4b62cfc91d
Sam Gleske <sam.mxracer@gmail.com>
2019-11-22 06:23:04 -0500

    Cleanup old files

--- scripts/wait-for-ready.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#Created by Sam Gleske
-nexus_url="${1:-http://localhost:8081}"
-until curl -fsSIu admin:admin123 -m 15 -o /dev/null \
-      "${nexus_url%/}"/service/metrics/ping; do
-  sleep 5
-done
